942ce242c3783ba94595dd32b8be74d4512a3476
Lyrise <lyrise1984@gmail.com>
2020-06-27 21:56:21 +0900

    fix: diskのパスを明示的に指定するように変更した

--- watch_disk_smart.sh
+++ watch_disk_smart.sh
@@ -1,8 +1,10 @@
+#/bin/bash
+
 while true; do
-    for v in $(lshw -short | grep disk | awk '{print $2}' | sort | uniq); do
+    while read v; do
         timestamp=$(date -u "+%Y-%m-%dT%H:%M:%S.000Z")
         temperature_celsius="$(smartctl -A $v | grep 'Temperature_Celsius' | awk '{print $10}')"
         jo "sender"="disk_smart" "type"="temperature_celsius" "timestamp"="$timestamp" "disk_path"="$v" "temperature_celsius"="$temperature_celsius" |tee -a "log/disk_smart.log"
-    done
+    done < ./.disks
     sleep 300
 done
